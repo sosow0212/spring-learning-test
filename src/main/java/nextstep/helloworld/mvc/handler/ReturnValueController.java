979ee9f4f3d465451c216d594415c59da0ffa6f7
sosow0212 <sosow0212@naver.com>
2023-04-24 17:41:18 +0900

    feat: Spring Core 학습

--- src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
+++ src/main/java/nextstep/helloworld/mvc/handler/ReturnValueController.java
@@ -1,32 +1,40 @@
 package nextstep.helloworld.mvc.handler;
 
 import nextstep.helloworld.mvc.domain.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/return-value")
 public class ReturnValueController {
 
-//    public void string() {
-//        return "message";
-//    }
-//
-//    public void responseBodyForUser() {
-//        return new User("name", "email");
-//    }
-//
-//    public void responseEntity(@PathVariable Long id) {
-//        return ResponseEntity.ok(new User("name", "email"));
-//    }
-//
-//    public void responseEntityFor400() {
-//        return ResponseEntity.badRequest().build();
-//    }
-//
-//    public void thymeleaf() {
-//        return "sample";
-//    }
+    @ResponseBody
+    @GetMapping("/message")
+    public String string() {
+        return "message";
+    }
+
+    @ResponseBody
+    @GetMapping("/users")
+    @ResponseStatus(HttpStatus.OK)
+    public User responseBodyForUser() {
+        return new User("name", "email");
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> responseEntity(@PathVariable Long id) {
+        return ResponseEntity.ok(new User("name", "email"));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<Object> responseEntityFor400() {
+        return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("/thymeleaf")
+    public String thymeleaf() {
+        return "sample";
+    }
 }
