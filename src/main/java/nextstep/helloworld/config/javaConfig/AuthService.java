e6778f1a699f76ba7caf975ade7cf79880e3a4b9
sosow0212 <sosow0212@naver.com>
2023-05-15 14:38:54 +0900

    feat: Configuration 학습 테스트

--- src/main/java/nextstep/helloworld/config/javaConfig/AuthService.java
+++ src/main/java/nextstep/helloworld/config/javaConfig/AuthService.java
@@ -1,5 +1,7 @@
 package nextstep.helloworld.config.javaConfig;
 
+import org.springframework.stereotype.Service;
+
 public class AuthService {
     public String findMemberName() {
         return "사용자";
